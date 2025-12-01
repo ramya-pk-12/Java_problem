bcc472ece908e130eb30816882189c73efbaeffa
ramya-pk-12 <ramya12112004@gmail.com>
2025-12-01 11:08:30 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        
+        int n = String.valueOf(num).length();  
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, n);
+            num /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+}
