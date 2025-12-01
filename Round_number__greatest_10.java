cc6c8a3854b73ff8e20dc210a279fde1fa57b6a1
ramya-pk-12 <ramya12112004@gmail.com>
2025-12-01 10:03:29 +0530

    Add files via upload

---
+++ Round_number__greatest_10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double num1=sc.nextDouble();
+        
+        double result = Math.ceil(num1);
+
+        System.out.println((int)result);
+       
+    }
+}
